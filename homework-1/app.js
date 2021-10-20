a15ea4adac3280897bb8418a846b82dfcd677cf8
Oleg Rostovskyi <rostovskiyoleg88@gmail.com>
2021-10-20 18:15:13 +0300

    final

--- homework-1/app.js
+++ homework-1/app.js
@@ -1,2 +1,19 @@
-let app = document.getElementById('app');
-app.innerHTML = 'Hello';
+let local = document.getElementById('local');
+let london = document.getElementById('london');
+
+function getTime() {
+  const today = new Date();
+  const timeLocal = today.toLocaleString('en-US', {
+    timeStyle: 'medium',
+    hourCycle: 'h24',
+  });
+  const timeLondon = today.toLocaleString('en-US', {
+    timeZone: 'Europe/London',
+    timeStyle: 'medium',
+    hourCycle: 'h24',
+  });
+  local.innerHTML = timeLocal;
+  london.innerHTML = timeLondon;
+}
+
+let clock = setInterval(getTime, 1000);
