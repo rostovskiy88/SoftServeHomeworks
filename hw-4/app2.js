6089d5424ca3aaaf48c159e037c34f805a0ebe4e
Oleg Rostovskyi <rostovskiyoleg88@gmail.com>
2021-11-01 12:00:37 +0200

    weather app

--- hw-4/app2.js
+++ hw-4/app2.js
@@ -1,10 +1,10 @@
-let EncryptedMessage =
-  'Yesterday, we bumped into Laura. It had to happen, but you cant deny the timing couldn\'t be worse. The "mission" to try and seduce her was a complete failure last month. By the way, she still has the ring I gave her. Anyhow, it hasn\'t been a pleasurable experience to go through it. I wanted to feel done with it first. Yesterda2, we bumped into Laura. It had to happen, but you cant deny the timing couldn\'t be worse. The "mission" to try and seduce her was a complete failure last month. By the way, she still has the ring I gave her. Anyhow, it hasn\'t been a pleasurable experience to go through it. I wanted to feel done with it first. Yesterda3, we bumped into Laura. It had to happen, but you cant deny the timing couldn\'t be worse. The "mission" to try and seduce her was a complete failure last month. By the way, she still has the ring I gave her. Anyhow, it hasn\'t been a pleasurable experience to go through it. I wanted to feel done with it first.';
+var string =
+  "Yesterday, we bumped into Laura. It had to happen, but you can't deny the timing couldn't be worse. The \"mission\" to try and seduce her was a complete failure last month. By the way, she still has the ring I gave her. Anyhow, it hasn't been a pleasurable experience to go through it. I wanted to feel done with it first. Actually, forget I said that. It was probably for the best - staying on this forever wasn't going lead anywhere good. I mean, I should not hold on to it forever. For what it's worth, I'm glad it forced me to get out of this dreamy illusion. A lesson for further down the road. A sort of instructions manual for life is what these past weeks have been, and it was all thanks to her.";
 
-function decryptMessage(message) {
+function decryptMessage(string) {
   // split into sentences and push to array
   let sentencesArray = [];
-  sentencesArray = message.match(/[^\.!\?]+[\.!\?]+/g);
+  sentencesArray = string.match(/[^\.!\?]+[\.!\?]+/g);
   //console.log(sentencesArray);
   let allSentences = [];
   let wordFirstLength = [];
@@ -12,7 +12,7 @@ function decryptMessage(message) {
   let wordCountRest = [];
   let mymessage;
 
-  for (let i = 0; i < message.length; i++) {
+  for (let i = 0; i < string.length; i++) {
     //sentencesArray = message.match(/[^\.!\?]+[\.!\?]+/g);
     for (let i = 0; i < sentencesArray.length; i++) {
       sentencesArray[i].match(/\b(\w+)\b/g);
@@ -26,7 +26,7 @@ function decryptMessage(message) {
     // divide fisrt and rest sentences
     let firstSentence = allSentences[0];
     if (firstSentence === undefined) return mymessage;
-    console.log(firstSentence); // check if different parts of encrypted message involved
+    //console.log(firstSentence); // check if different parts of encrypted message involved
     allSentences.shift();
     // count number of words
     let wordCountFirst = firstSentence.match(/(\w+)/g);
@@ -48,5 +48,4 @@ function decryptMessage(message) {
   return mymessage;
 }
 
-console.log(decryptMessage(EncryptedMessage));
-// 'The mission has been done.' x3  //result
+console.log(decryptMessage(string));
